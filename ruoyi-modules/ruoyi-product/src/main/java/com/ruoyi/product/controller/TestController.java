b1ea0cd16f26f16d86cdfe58e07205d2320ecb8c
wnagjiacang <2927707425@qq.com>
2024-02-27 16:20:50 +0800

    目录切换

--- ruoyi-modules/ruoyi-product/src/main/java/com/ruoyi/product/controller/TestController.java
+++ ruoyi-modules/ruoyi-product/src/main/java/com/ruoyi/product/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.ruoyi.controller;
+package com.ruoyi.product.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
