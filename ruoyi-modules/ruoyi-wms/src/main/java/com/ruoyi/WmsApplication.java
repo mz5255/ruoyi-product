70d7cbe87e21d845525df773d9353160d59234b2
刹那 <13357428+mz5255@user.noreply.gitee.com>
2024-03-05 11:33:11 +0800

    冲突测试2

---
+++ ruoyi-modules/ruoyi-wms/src/main/java/com/ruoyi/WmsApplication.java
@@ -0,0 +1,13 @@
+package com.ruoyi;
+
+/**
+ * Hello world!
+ *
+ */
+public class WmsApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
